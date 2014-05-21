5df5f22a1dff6478432b6b6e9813f33ef7148204
juanx96506@gmail.com <juanx96506@gmail.com@6e422297-26b1-447a-7b00-40e2b1d2efed>
2014-05-21 23:52:56 +0000

    Arreglando branch

--- branches/Juan DIego Gonzalez/build/web/js/informes.js
+++
@@ -1,8 +0,0 @@
-$("document").ready(function() {
-    var fecha;
-    var f = new Date();
-    fecha= f.getFullYear() + "-" + (f.getMonth() +1) + "-" + f.getDate();
-     $("#fecha2").val(fecha);
-     $("#fecha2").attr("max",fecha);
-     console.log(fecha);
-});
