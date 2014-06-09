e4d7e4fdfcee7fd56e1c5b399439fd3af810a0d4
juanx96506@gmail.com <juanx96506@gmail.com@6e422297-26b1-447a-7b00-40e2b1d2efed>
2014-06-09 22:50:41 +0000

    Agregados nuevos metodos, al usar Listas ligadas

---
+++ trunk/actividas/src/java/Entidades/List.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author juanx96506
+ * @param <T>
+ */
+public class List <T>{
+
+    Nodo<T> first;
+    Nodo<T>  last;
+    
+    public List(){
+        first = null;
+        last = null;
+    }
+
+    public void setFirst(Nodo<T> first) {
+        this.first = first;
+    }
+
+    public void setLast(Nodo<T> last) {
+        this.last = last;
+    }
+
+    public Nodo<T> getFirst() {
+        return first;
+    }
+
+    public Nodo<T> getLast() {
+        return last;
+    }
+
+    //Pre-constructor de addLast
+    public void add(T dato){
+		addLast(dato);
+	}
+    
+    
+    //Insertar al final de la lista
+    public void addLast(T a) {
+        Nodo insert = new Nodo(a);
+        if (isEmpty()) {
+            first = insert;
+            last = insert;
+        } else {
+            last.setNext(insert);
+            insert.setBack(last);
+            last = insert;
+        }
+    }
+    
+    public boolean Delete(Nodo search){
+        boolean find = false;
+        Nodo aux = first;
+        for(;aux != null;aux = aux.getNext()){
+            if(aux.getDato().equals(search.getDato())){
+                aux.getBack().setNext(aux.getNext());
+                aux.getNext().setBack(aux.getBack());
+                find = true;
+                aux = null;
+                break;
+            }
+        }   
+        return find;
+    }
+
+    //Verifica si la lista esta vacia
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    
+    //Nos da a conocer el tamaño de la lista
+    public int size() {
+        int cont = 0;
+        if (isEmpty()) {
+            cont = -1;
+        } else {
+            Nodo<T> temp = first;
+            while (temp != null) {
+                cont++;
+                temp = temp.getNext();
+            }
+        }
+        return cont;
+    }
+
+}
--- trunk/actividas/src/java/Negocio/ClienteN.java
+++ trunk/actividas/src/java/Negocio/ClienteN.java
@@ -14,29 +14,29 @@ import Utilidades.Conexion;
 public class ClienteN {
 
     public DaosCliente dao;
-    public static List <ClienteE> head = null;
-    
-    
+    public static List<ClienteE> head = null;
+
     public ClienteN() {
         dao = new DaosCliente();
-        if(head==null){
-            head=getList();
+        if (head == null) {
+            head = getList();
             System.out.println("lista llenada");
         }
     }
-    
-    public List<ClienteE> getList(){
+
+    public List<ClienteE> getList() {
         List<ClienteE> aux = new List();
         Conexion con = new Conexion();
         Connection c = con.getCon();
         aux = dao.fillList(c, "cedula", null, false);
         return aux;
     }
-    
+
     public String crearCliente(ClienteE ee) {
         Conexion con = new Conexion();
         Connection c = con.getCon();
-        String s = dao.crearCliente(c, ee);
+        //    String s = dao.crearCliente(c, ee);
+        String s = dao.crearCliente(c, ee, head);
         return s;
     }
 
@@ -45,14 +45,29 @@ public class ClienteN {
         Connection c = con.getCon();
         return dao.buscarCliente(c, variable, valor, exactamente);
     }
+    
+    //Usando listas ligadas
+   /* public String actualizarCliente(ClienteE ce) {
+        Conexion con = new Conexion();
+        return dao.actualizarCliente(con.getCon(), ce, head);
+    }*/
 
-    public String actualizarCliente(ClienteE ce) {
+    //Usando conexion directa a la DB
+   public String actualizarCliente(ClienteE ce) {
         Conexion con = new Conexion();
         return dao.actualizarCliente(con.getCon(), ce);
     }
 
-    public String eliminarCliente(String cedula) {
+    //Usando listas ligadas
+   /* public String eliminarCliente(ClienteE clie) {
+        Conexion con = new Conexion();
+        return dao.eliminarCliente(con.getCon(), clie, head);
+    }*/
+    
+    //Usando conexion directa a la DB
+public String eliminarCliente(String cedula) {
         Conexion con = new Conexion();
         return dao.eliminarCliente(con.getCon(), cedula);
     }
+
 }
