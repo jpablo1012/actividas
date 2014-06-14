4b7d3fda970ca764639ed265856e4a2479dada7b
oscarbuitrago9001 <oscarbuitrago9001@6e422297-26b1-447a-7b00-40e2b1d2efed>
2014-06-14 12:51:04 +0000

    validacion fecha en crear pedido; Informes web.

--- actividas/web/js/informes.js
+++ actividas/web/js/informes.js
@@ -1,8 +1,28 @@
 $("document").ready(function() {
+    fecha();
+
+    $("#infefech").click(function() {
+        validar();
+        return false;
+    });
+});
+
+function validar() {
+    console.log("lala");
+}
+
+function fecha() { // coloca la fecha por defecto y el max
     var fecha;
     var f = new Date();
-    fecha= f.getFullYear() + "-" + (f.getMonth() +1) + "-" + f.getDate();
-     $("#fecha2").val(fecha);
-     $("#fecha2").attr("max",fecha);
-     console.log(fecha);
-});
+    fecha = f.getFullYear() + "-";
+    if (f.getMonth() + 1 <= 9) {
+        fecha = fecha + "0";
+    }
+    fecha = fecha + (f.getMonth() + 1) + "-";
+    if (f.getDate() <= 9) {
+        fecha = fecha + "0";
+    }
+    fecha = fecha + f.getDate();
+    $("#fecha2").val(fecha);
+    $("#fecha2").attr("max", fecha);
+}
